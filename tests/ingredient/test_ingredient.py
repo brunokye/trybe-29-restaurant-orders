a6214dd8bae51bcd88801b80bab2d905b0124c9a
Bruno Ferreira <contatobrunokye@gmail.com>
2023-06-19 17:49:47 -0300

    feat: requisito 01

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,20 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Restriction
+from src.models.ingredient import Ingredient
 
 
 # Req 1
 def test_ingredient():
-    pass
+    tomate_01 = Ingredient('tomate')
+    tomate_02 = Ingredient('tomate')
+    farinha = Ingredient('farinha')
+
+    assert farinha.restrictions == {Restriction.GLUTEN}
+
+    assert tomate_01.__hash__() == tomate_02.__hash__()
+    assert tomate_01.__hash__() != farinha.__hash__()
+
+    assert tomate_01.__eq__(tomate_02) is True
+    assert tomate_01.__eq__(farinha) is False
+
+    name_test = farinha.name
+    assert farinha.__repr__() == f"Ingredient('{name_test}')"
